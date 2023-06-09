f31e827795929c1e3ebdfdcc52d4b364b090fe44
SilverLine <silverlinevault@gmail.com>
2023-06-09 19:44:46 +0200

    Vault some major improvements

--- src/Vault.sol
+++ src/Vault.sol
@@ -7,54 +7,75 @@ import {Ownable} from 'openzeppelin-contracts/access/Ownable.sol'; // allows usa
 import {ERC20} from 'openzeppelin-contracts/token/ERC20/ERC20.sol';
 import {ILido} from './interfaces/ILido.sol';
 
+/**
+    * @title HarvestManager
+    * @dev HarvestManager contract
+    * @notice HarvestManager is a contract that recieves earned staking rewards from the Vault contract 
+    and distributes them to the beneficaires
+*/
+
 contract Vault is Ownable, ERC20 {
 
-    // Initiate Lido interface
+    /// @notice Initiate Lido interface
     ILido public lidoContract;
 
-    address public rewardAddress;
+    /// @notice  
     address public vaultOwner;
+
+    /// @notice    
     address public harvestManager;
+    
+    /// @notice 
     bool public emergencyMode;
     
+    /// @notice Event fired when owner of the contract is changed
     event OwnershipTransfered(address newOwner);
 
+    /// @notice Event fired when contract of Harvest manager is changed
     event ManagerChanged(address newManager);
 
-    constructor(address _token, address _lidoContractAddress, address _rewardAddress) ERC20("Impact ETH", "imETH") {
-
-        rewardAddress = _rewardAddress;
+    constructor(address _token, address _lidoContractAddress) ERC20("Impact ETH", "imETH") {
         lidoContract = ILido(_lidoContractAddress);
-
-        _transferOwnership(_msgSender());
-
+        transferOwnership(_msgSender());
         harvestManager = msg.sender;
         emergencyMode = false;
-
     }
 
+    /**
+        * @notice 
+    */
+    function calculateRewards() public view returns (uint) {
+        uint _undistributedRewards;
+        _undistributedRewards = uint(stETH.balanceOf(address(this))) - uint(address(this).balance);
+        return _undistributedRewards;
+    }
 
     ////////////////////////////////////
     /// USER FUNCTIONS
-    /// https://solidity-by-example.org/defi/vault/
     /////////////////////////////////////
 
-    function deposit() external payable {
-        _mint(msg.sender, msg.value);
+    /**
+        * @notice Function that sends ETH to Lido to obtain stETH
+    */
+    function stakeToLido() internal payable {
+        lidoContract.submit{value: msg.value};
     }
 
+    /**
+        * @notice Function that enables to the user to deposit ETH in order to donate related rewards from staking
+    */
+    function deposit() external payable {
+        _mint(msg.sender, msg.value);
+        stakeToLido();
+    }
+
+    /**
+        * @notice Function that enables to the user to withdraw their funds  
+        * @param _amountToWithdraw amount that a user wants to withdraw
+    */
     function withdraw(uint _amountToWithdraw) external {
         require((this.balanceOf(msg.sender)>= _amountToWithdraw), 'You cannot withdraw more than you deposited');
         _burn(msg.sender, _amountToWithdraw);
-        
-        /*
-        To be done
-        token.transfer(msg.sender, _amountToWithdraw);
-        */
-    }
-
-    function stakeToLido() external payable {
-        lidoContract.submit{value: msg.value}(rewardAddress);
     }
 
     function harvestRewards() external {
@@ -80,7 +101,6 @@ contract Vault is Ownable, ERC20 {
     /** 
         @notice Enables to put contract into 'Emergency mode' thus it is not possible to deposit any funds
     */
-
     function activateEmergency() public onlyOwner{
         emergencyMode = true;
     }
@@ -88,7 +108,6 @@ contract Vault is Ownable, ERC20 {
     /** 
         @notice Enables to send rest of funds that are not possible to withdraw to the Harvest Manager contract
     */
-
     function sweep() external onlyOwner {
 
     }
